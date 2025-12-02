c8cc6eff225faadaf7d3580efbd8053e4c655a54
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2025-12-03 06:07:41 +0700

    feat: implement auto_save_session callback for root_agent to enhance session management

--- ai/src/agent.py
+++ ai/src/agent.py
@@ -1,5 +1,13 @@
 from google.adk import Agent
+from google.adk.agents.callback_context import CallbackContext
 from google.adk.tools.preload_memory_tool import PreloadMemoryTool
+
+
+async def auto_save_session(callback_context: CallbackContext):
+    await callback_context._invocation_context.memory_service.add_session_to_memory(
+        callback_context._invocation_context.session
+    )
+
 
 root_agent = Agent(
     name="root_agent",
@@ -7,4 +15,5 @@ root_agent = Agent(
     description="A helpful assistant for user questions.",
     instruction="Answer user questions to the best of your knowledge",
     tools=[PreloadMemoryTool()],
+    after_agent_callback=[auto_save_session],
 )

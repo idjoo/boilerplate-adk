2bd38bb35a6c62c1c36c6e3525162ed44dfdddf4
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2025-12-19 09:05:05 +0700

    feat(ai): implement the new interaction api

--- ai/src/agent.py
+++ ai/src/agent.py
@@ -1,8 +1,14 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.tools import google_search
 
 root_agent = Agent(
     name="General",
-    model="gemini-3-pro-preview",
+    model=Gemini(
+        model="gemini-3-pro-preview",
+        use_interactions_api=True,
+    ),
     description="A helpful assistant for user questions.",
     instruction="Answer user questions to the best of your knowledge",
+    tools=[google_search],
 )
